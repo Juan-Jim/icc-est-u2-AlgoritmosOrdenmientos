fec1808d8c96c65993e0bf7df08fea4062108bc2
duckers <SZIndustry@github.com>
2025-05-10 21:14:29 -0500

    sinc

--- src_py/app.py
+++ src_py/app.py
@@ -4,33 +4,15 @@ import benchmarking as bm
 from metodos_ordenamientos import Metodo_ordenamiento
 
 if __name__ == "__main__":
-    ##print("Funciona")
-    # bm.Benchmarking()
-    #Benchmarking()
-    bench = bm.Benchmarking()
-    metodosO = Metodo_ordenamiento()
+    mO = Metodo_ordenamiento
+
+    tamanos = list[int]
+    max_tamano = 50000
     
-    ##tam = 10000
-    tamanios = [5000, 10000, 20000]
-    resultados = []
-    
-    for tam in tamanios:
-        arreglo_base = bench.build_arreglo(tam)
-
-        metodos_dic = {
-            "burbuja": metodosO.metodo_burbuja,
-            "burbujaM": metodosO.sort_burbuja_mejorado_optimizado,
-            "seleccion": metodosO.sort_seleccion,
-            "shell": metodosO.sort_shell,
-        }
-
-    
-
-        for nombre , fun_metodo in metodos_dic.items():
-            tiempo_resultado = bench.medir_tiempo(fun_metodo, arreglo_base)
-            tupla_resultado = (tam, nombre, tiempo_resultado)
-            resultados.append(tupla_resultado)
-
-    for tam, nombre, tiempo in resultados:
-        print(f'Tamano: {tam}, nombre metodo: {nombre}, tiempo: {tiempo:.6f} segundos')
-
+    self.arreglo_base: list[int] = []
+    self.algoritmos: metodos[str, Callable] = {
+        "Bubble Sort": mO.metodo_burbuja,
+        "Burbuja mejorado": mO.sort_burbuja_mejorado_optimizado,
+        "Selection Sort": mO.sort_seleccion,
+        "Shell Sort": mO.sort_shell
+    }
